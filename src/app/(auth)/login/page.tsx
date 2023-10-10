72212fb590ab5f812f4e084ac615261e66959241
Anthony Huerta <anthonyhuerta@Anthonys-MacBook-Air.local>
2023-10-10 11:41:46 -0500

    fixed production build errors

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -129,7 +129,7 @@ const Page: FC = () => {
                 </button>
 
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don't have an account yet?{" "}
+                  Dont have an account yet?{" "}
                   <a
                     href="/register"
                     className="font-medium text-primary-600 hover:underline dark:text-primary-500"
