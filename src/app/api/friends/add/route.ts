45c761803f27cdeb6ada6c73535485544b889078
ajhuerta <ajhuerta6@gmail.com>
2024-12-29 20:50:44 -0600

    fixing response error

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -69,9 +69,9 @@ export async function POST(req: Request) {
 
     } catch (error){
         if(error instanceof z.ZodError){
-            return new Response("Invalid request payload"), {status: 422}
+            return new Response("Invalid request payload", {status: 422})
         }
 
-        return new Response('Invalid request'), { status: 400}
+        return new Response('Invalid request', { status: 400})
     }
 }
